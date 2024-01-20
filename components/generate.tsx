8e34d42eaca62714600eedf6c4dc38ed7e4794fb
Denise Schleier <denise.schleier@outlook.com>
2024-01-20 15:55:22 -0800

    use cohere

--- components/generate.tsx
+++ components/generate.tsx
@@ -2,13 +2,34 @@
 
 import { useEffect, useState } from 'react'
 import styles from './generate.module.css'
+import { useConfig } from 'nextra-theme-docs'
+
+import { useCohere, useKickTemplate } from '../modules/kick-it/src'
+import type { KickTemplate } from '../modules/kick-it/src'
 
 function MyButton() {
     const [ markdown, setMarkdown ] = useState('')
+    const config = useConfig()
 
-    function handleClick() {
-        setMarkdown('markdown')
-        console.log(markdown)
+    useEffect(() => {
+    })
+
+    async function handleClick() {
+        const templ: KickTemplate = useKickTemplate({
+            constants: [
+                {
+                    key: 'name',
+                    value: 'John Moe'
+                }
+            ],
+            contents: [
+                'Hello, {name}!'
+            ]
+        })
+
+        const result = await useCohere(templ)
+
+        setMarkdown(result)
     }
 
     return (
@@ -22,6 +43,11 @@ function MyButton() {
     )
 }
 
-export default function Generate() {
+interface Props {
+    children?: React.ReactNode
+}
+
+export default function Generate(props: Props) {
+    console.log(props)
     return <MyButton />
 }
