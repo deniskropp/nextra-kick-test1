1ee5a2c90498dfd230ec76a2d9c750f4a0bc4f03
Denise Schleier <denise.schleier@outlook.com>
2024-03-22 13:46:51 -0700

    cleanup

--- pages/api/cohere.ts
+++ pages/api/cohere.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { useCohere, useKickTemplate } from '../../modules/kick-it/src'
-import { AxiosError } from 'axios'
 
 export default async function cohere(request: NextApiRequest, response: NextApiResponse) {
     try {
